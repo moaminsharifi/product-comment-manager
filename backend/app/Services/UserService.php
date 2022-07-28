ac1d102e90c9ed400232570a2e816c5de3608ad7
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-29 04:27:44 +0430

    - // Add UserRepository as local $repository variable Update getAll function Update getById function Update deleteById function Update create function Update update function Update updatePassword function Update login function

--- backend/app/Services/UserService.php
+++ backend/app/Services/UserService.php
@@ -2,28 +2,119 @@
 
 namespace App\Services;
 
+use App\Contracts\UserServiceInterface;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdatePasswordUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
+use App\Repositories\UserRepository;
 use Illuminate\Database\Eloquent\Collection;
-
-class UserService extends Service
+use Illuminate\Support\Facades\Hash;
+use App\Helpers\CustomResponse;
+class UserService extends Service implements UserServiceInterface
 {
+    /**
+     * Constructor function.
+     *
+     * @param UserRepository $repository
+     */
+    public function __construct(UserRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * get all users
+     *
+     * @return Collection
+     */
     public function getAll(): Collection
     {
+        return $this->repository->getAll();
     }
-
+    /**
+     * get user by is
+     *
+     * @param integer $id
+     * @return User
+     */
     public function getById(int $id): User
     {
+        return $this->repository->getById($id);
     }
 
+    /**
+     * Delete user
+     *
+     * @param integer $id
+     * @return boolean
+     */
     public function deleteById(int $id): bool
     {
+        return $this->repository->deleteById($id);
     }
 
-    public function create(array $attributes): User
+    /**
+     * Create new user
+     *
+     * @param StoreUserRequest $request
+     * @return User
+     */
+    public function create(StoreUserRequest $request): User
     {
-    }
+        $request->validate();
+        $attributes = $request->all();
+        $attributes['password'] = Hash::make($attributes['password']);
 
-    public function update(int $id, array $attributes): User
+        return $this->repository->create($attributes);
+    }
+    /**
+     * update user fields
+     *
+     * @param User $user
+     * @param UpdateUserRequest $request
+     * @return User
+     */
+    public function update(User $user, UpdateUserRequest $request): User
     {
+        $request->validate();
+        $attributes = $request->only(['name']);
+
+        return $this->repository->update($user->id, [
+            'name'=>$attributes['name']
+        ]);
+    }
+    /**
+     * update user Password
+     *
+     * @param User $user
+     * @param UpdatePasswordUserRequest $request
+     * @return User
+     */
+    public function updatePassword(User $user, UpdatePasswordUserRequest $request): User
+    {
+        $request->validate();
+        $attributes = $request->only(['password', 'old_password']);
+        abort_unless(Hash::check($attributes['old_password'], $user->password), CustomResponse::createError('10002'));
+        return $this->repository->update($user->id, [
+            'password'=>Hash::make($attributes['password'])
+        ]);
+    }
+    /**
+     * login new user
+     *
+     * @param LoginUserRequest $request
+     * @return User
+     */
+    public function login(LoginUserRequest $request): User
+    {
+        $request->validate();
+        $attributes = $request->only(['email', 'password']);
+        $user = $this->repository->getByEmail($attributes['email']);
+        abort_unless($user, CustomResponse::createError('10001') );
+        abort_unless(Hash::check($attributes['password'], $user->password), CustomResponse::createError('10002') );
+
+        return $user;
     }
 }
