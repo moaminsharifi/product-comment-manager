752b61c1a470b38d54434c7b4bb30c39468502f0
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-29 04:31:10 +0430

    - // Add route of basic auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [UserController::class,'login'])->name('login');
+    Route::post('signup', [UserController::class,'signup'])->name('signup');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group([
+        'middleware' => 'auth:sanctum'
+    ], function() {
+        Route::get('user', [UserController::class,'show'])->name('userData');
+    });
 });
