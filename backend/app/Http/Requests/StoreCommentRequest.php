405ec9b2aa7b1a377a5e4de91c5c92c1b23ebb16
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-29 17:33:49 +0430

    - // Update validate StoreCommentRequest rules

--- backend/app/Http/Requests/StoreCommentRequest.php
+++ backend/app/Http/Requests/StoreCommentRequest.php
@@ -13,7 +13,7 @@ class StoreCommentRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StoreCommentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'productName'=>'required|string|max:180|min:1',
+            'comment'=> 'required|string|max:10000|min:1',
         ];
     }
 }
