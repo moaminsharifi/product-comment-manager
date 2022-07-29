cb312237a10e4616dc2f358b56f04d9eb8c67a0d
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-29 15:21:53 +0430

    - // Update validate email in the rules

--- backend/app/Http/Requests/LoginUserRequest.php
+++ backend/app/Http/Requests/LoginUserRequest.php
@@ -24,7 +24,7 @@ class LoginUserRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|string|email',
+            'email' => 'required|string|email|exists:users,email',
             'password' => 'required|string|max:180',
         ];
     }
