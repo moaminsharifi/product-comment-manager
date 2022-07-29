e69d53ef32344761bc7b637e3aba1c0fd5830dc5
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-29 21:38:03 +0430

    - // Add Seed some data

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +18,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        // - Add this user with factory: (user: parspack, pass: random and safe password)
+        $user = User::factory()->create([
+        'password'=>Hash::make('RADOM_6004d5f8-d8a6-3e95-9ad2-811663edbecf_PASSWORD'),
+        'email'=> 'parspack@parspack.com',
+        'name'=>'parspack']);
+        $productA = Product::factory()->create(['name'=>'A',
+        'creator_id'=>$user->id]);
+
+        // - Add below products by default: A, B, C
+        $productB = Product::factory()->create(['name'=>'B',
+        'creator_id'=>$user->id]);
+        $productC = Product::factory()->create(['name'=>'C',
+        'creator_id'=>$user->id]);
+      
+       
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
