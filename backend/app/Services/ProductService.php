c175bd7b7ddb02d6280f3f00159fe6adfba444ec
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-29 17:38:00 +0430

    - // Add ProductService

---
+++ backend/app/Services/ProductService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\ProductServiceInterface;
+use App\Helpers\CustomResponse;
+use App\Http\Requests\LoginProductRequest;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdatePasswordProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+use App\Repositories\ProductRepository;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Hash;
+
+class ProductService extends Service 
+{
+    /**
+     * Constructor function.
+     *
+     * @param ProductRepository $repository
+     */
+    public function __construct(ProductRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * get all users.
+     *
+     * @return Collection
+     */
+    public function getAll(): Collection
+    {
+        return $this->repository->getAll();
+    }
+
+    /**
+     * get user by is.
+     *
+     * @param int $id
+     * @return Product
+     */
+    public function getById(int $id): Product
+    {
+        return $this->repository->getById($id);
+    }
+
+    /**
+     * Delete user.
+     *
+     * @param int $id
+     * @return bool
+     */
+    public function deleteById(int $id): bool
+    {
+        return $this->repository->deleteById($id);
+    }
+
+   
+}
