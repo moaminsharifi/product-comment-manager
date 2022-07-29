ae9bb96a4e5b37150ed2bbbf601c6b3a3fc0af76
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-29 05:32:26 +0430

    - // Update add default authorize and rules

--- backend/app/Http/Requests/LoginUserRequest.php
+++ backend/app/Http/Requests/LoginUserRequest.php
@@ -13,7 +13,7 @@ class LoginUserRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class LoginUserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'email' => 'required|string|email',
+            'password' => 'required|string|max:180',
         ];
     }
 }
--- backend/app/Http/Requests/StoreUserRequest.php
+++ backend/app/Http/Requests/StoreUserRequest.php
@@ -13,7 +13,7 @@ class StoreUserRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class StoreUserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:180|min:3',
+            'email' => 'required|string|email|unique:users|max:180',
+            'password' => 'required|string|max:180|min:6',
+            'password_confirm'=> 'required|same:password|max:180'
         ];
     }
 }
