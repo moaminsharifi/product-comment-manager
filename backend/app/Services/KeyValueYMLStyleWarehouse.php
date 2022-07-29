2b809571e4632505daa33e3e4ff6bd6ff67b8ce5
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-30 02:56:24 +0430

    - // Add KeyValueYMLStyleWarehouse

---
+++ backend/app/Services/KeyValueYMLStyleWarehouse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+class KeyValueYMLStyleWarehouse extends BaseWarehouse
+{
+
+    /**
+     * increment.
+     *
+     * @param string $key
+     * @return void
+     */
+    public function increment(string $key)
+    {
+        
+    }
+    /**
+     * get specific key
+     *
+     * @param string $key
+     * @return value
+     */
+    public function get(string $key){
+       
+    }
+
+}
