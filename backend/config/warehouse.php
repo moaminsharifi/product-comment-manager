49ac768be416172bf6118371b72da7df98c4c88a
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-30 02:17:36 +0430

    - // Update warehouse configs

--- backend/config/warehouse.php
+++ backend/config/warehouse.php
@@ -1,27 +1,13 @@
 <?php
-
+use Illuminate\Support\Str;
 return [
 
     'connections' => [
-        'redis' => [
-
-            'client' => env('REDIS_CLIENT', 'phpredis'),
-
-            'options' => [
-                'cluster' => env('REDIS_CLUSTER', 'redis'),
-                'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_warehouse'),
-            ],
-
-            'default' => [
-                'url' => env('REDIS_URL'),
-                'host' => env('REDIS_HOST', '127.0.0.1'),
-                'username' => env('REDIS_USERNAME'),
-                'password' => env('REDIS_PASSWORD'),
-                'port' => env('REDIS_PORT', '6379'),
-                'database' => env('REDIS_DB', '0'),
-            ],
-
-        ],
+        'redis' => 'default',
+        'json' => [
+            'driver' => 'file',
+            'path' => 'warehouse.json',
+        ]
 
     ],
 ];
