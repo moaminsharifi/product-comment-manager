b6342e370f3e9eaaed6217750da0ba24ba3b4aaa
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-30 01:37:18 +0430

    - // Update to use predis package

--- backend/config/database.php
+++ backend/config/database.php
@@ -121,7 +121,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -129,7 +129,6 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
