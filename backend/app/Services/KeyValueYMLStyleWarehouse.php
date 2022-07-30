ffa4f1484af4fd2c1f0cb735541ce2d9e92041b0
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-30 14:31:52 +0430

    - // Add KeyValueYMLStyle functionality Add KeyValueYMLStyleTest

--- backend/app/Services/KeyValueYMLStyleWarehouse.php
+++ backend/app/Services/KeyValueYMLStyleWarehouse.php
@@ -1,18 +1,47 @@
 <?php
 
 namespace App\Services;
-
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\ProcessBuilder;
 class KeyValueYMLStyleWarehouse extends BaseWarehouse
 {
-
+    protected function runCommand($command)
+    {
+        $output=null;
+        $retval=null;
+        exec($command, $output, $retval);
+        return $output;
+    }
     /**
      * increment.
      *
      * @param string $key
      * @return void
      */
-    public function increment(string $key)
-    {
+    public function increment(string $key, $filePath = false)
+    {       
+        if(!$filePath){
+            $filePath = storage_path(config('warehouse.connections.yml.path'));
+        }
+        $command = "grep {$key} {$filePath}";
+        $output = $this->runCommand($command);
+        $existInFile =  count($output) ? true : false;
+
+        if ($existInFile) {
+            // get oldValue
+            $valuesInFile = explode(":", $output[0]);
+            $oldValue = (int)$valuesInFile[1];
+            $newValue = $oldValue + 1;
+
+            $command = sprintf("sed -ir 's/^[#]*\s*%s: .*/%s: %u/' %s",$key,$key,$newValue,$filePath);
+            $output =  $this->runCommand($command);
+        } else {
+            $command = "echo '{$key}: 1' >> {$filePath}";
+            $output = $this->runCommand($command);
+        }
+        
+
         
     }
     /**
@@ -21,8 +50,19 @@ class KeyValueYMLStyleWarehouse extends BaseWarehouse
      * @param string $key
      * @return value
      */
-    public function get(string $key){
-       
+    public function get(string $key, $filePath = false){
+        if(!$filePath){
+            $filePath = storage_path(config('warehouse.connections.yml.path'));
+        }
+        try {
+            $command = sprintf('sed -rn "s/^%s: ([^\n]+)$/\1/p" %s',$key,$filePath);
+            $output = $this->runCommand($command);
+            return  $output[0];
+        } catch (\Throwable $th) {
+            return 0;
+        }
+        
+
     }
 
 }
---
+++ backend/tests/Feature/Warehouse/KeyValueYMLStyleTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature\Warehouse;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
+use App\Services\KeyValueYMLStyleWarehouse;
+class KeyValueYMLStyleTest extends TestCase
+{
+     /**
+     * add multi value to warehouse test
+     * @test
+     * @group Feature
+     * @group Warehouse
+     * @return void
+     */
+    public function add_multi_value_to_warehouse()
+    {
+        $warehouse = new KeyValueYMLStyleWarehouse();
+        $disk = 'warehouses';
+        $filePath = 'warehouse.yml';
+        Storage::fake($disk);
+        Storage::disk($disk)->put($filePath, '');
+        Storage::disk($disk)->assertExists($filePath);
+        $fileFullPath = Storage::disk($disk)->path($filePath);
+        $warehouse->increment('A',$fileFullPath);
+
+        $fileContent = Storage::disk($disk)->get($filePath);
+        $this->assertTrue(str_contains($fileContent, 'A: 1'));
+       
+
+        $warehouse->increment('A',$fileFullPath);
+        $fileContent = Storage::disk($disk)->get($filePath);
+        
+        $this->assertTrue(str_contains($fileContent, 'A: 2'));
+
+        // Add other key
+        $warehouse->increment('B',$fileFullPath);
+        $fileContent = Storage::disk($disk)->get($filePath);
+        $this->assertTrue(str_contains($fileContent, 'B: 1'));
+
+        $warehouse->increment('B',$fileFullPath);
+        $fileContent = Storage::disk($disk)->get($filePath);
+        $this->assertTrue(str_contains($fileContent, 'B: 2'));
+
+      
+    }
+
+
+    /**
+     * get value from warehouse test
+     * @test
+     * @group Feature
+     * @group Warehouse
+     * @return void
+     */
+    public function get_value_from_warehouse()
+    {
+        $warehouse = new KeyValueYMLStyleWarehouse();
+        $disk = 'warehouses';
+        $filePath = 'warehouse2.yml';
+        Storage::fake($disk);
+        Storage::disk($disk)->put($filePath, '');
+        Storage::disk($disk)->assertExists($filePath);
+        $fileFullPath = Storage::disk($disk)->path($filePath);
+        $warehouse->increment('A',$fileFullPath);
+
+        $fileContent = Storage::disk($disk)->get($filePath);
+        $this->assertTrue(str_contains($fileContent, 'A: 1'));
+       
+
+        $output = $warehouse->get('A',$fileFullPath);
+        $this->assertEquals(1 , $output);
+        
+      
+    }
+}
