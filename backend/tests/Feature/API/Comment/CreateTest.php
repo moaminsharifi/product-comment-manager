eb4941b7f59830772e8639d73fafc8ffd6592dec
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-30 16:15:51 +0430

    - // Add create new comment fire job test

--- backend/tests/Feature/API/Comment/CreateTest.php
+++ backend/tests/Feature/API/Comment/CreateTest.php
@@ -8,7 +8,8 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use Tests\TestCase;
-
+use Illuminate\Support\Facades\Bus;
+use App\Jobs\IncrementCommentOfProductJob;
 class CreateTest extends TestCase
 {
     use RefreshDatabase;
@@ -236,4 +237,49 @@ class CreateTest extends TestCase
             ]
         );
     }
+    /**
+     * create new comment fire job test
+     * @test
+     * @group API
+     * @group User
+     * @group Feature
+     * @return void
+     */
+    public function create_new_comment_fire_job()
+    {
+        Bus::fake();
+        // prepare data
+        $user = User::factory()->create();
+        $product = Product::factory()->create(['creator_id'=>$user->id]);
+        $userDataForLogin = [
+            'email'=>  $user->email,
+            'password'=>'password',
+        ];
+
+        // send request
+        $response = $this->json('POST', route('api.login'), $userDataForLogin);
+        // after send request assertion
+        $response->assertStatus(Response::HTTP_OK);
+
+        $token = json_decode($response->getContent())->data->token;
+        $header = [
+            'HTTP_Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+
+        $commentData = [
+            'productName'=>$product->name,
+            'comment'=> 'comment',
+        ];
+
+        $response = $this->json('POST', route('api.AddNewComment'), $commentData, $header);
+
+        $response->assertStatus(Response::HTTP_CREATED)
+        ->assertJsonStructure(
+            $this->jsonStructResponseData
+        );
+        Bus::assertDispatched(IncrementCommentOfProductJob::class);
+    }
+
 }
