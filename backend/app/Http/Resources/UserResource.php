f75c7547a7a93be63b37ab36814fffafc3ca8e71
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-30 17:07:06 +0430

    - // Change Api token type to passport

--- backend/app/Http/Resources/UserResource.php
+++ backend/app/Http/Resources/UserResource.php
@@ -18,7 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'token'=> $this->createToken('authToken')->plainTextToken,
+            'token'=> $this->createToken('authToken')->accessToken,
             'token_type' => 'Bearer',
         ];
     }
