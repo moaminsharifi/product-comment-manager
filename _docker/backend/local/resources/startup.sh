30775909d6b8687db2470e21bc6c06cb3cd1315c
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-31 03:07:22 +0430

    - // Add queue-worker supervisor setup for docker

--- _docker/backend/local/resources/startup.sh
+++ _docker/backend/local/resources/startup.sh
@@ -6,9 +6,9 @@ php artisan route:cache
 php artisan cache:clear
 php artisan config:clear
 php artisan optimize
-# php artisan passport:install
+php artisan passport:install
 #php artisan migrate --force
-
+service supervisor start 
 # chmod -R 777 /var/www/html/storage
 # php artisan octane:start --host=0.0.0.0 --port=8000
 php artisan serve --host=0.0.0.0 --port=8000
