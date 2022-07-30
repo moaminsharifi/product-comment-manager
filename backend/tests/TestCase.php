e1886800159f6e403f08f43d16ed6e5a8a0192c6
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-30 17:14:23 +0430

    - // Change test to compatible with laravel/passport

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    public function setUp() : void
+    {
+        parent::setUp();
+        \Artisan::call('migrate',['-vvv' => true]);
+        \Artisan::call('passport:install',['-vvv' => true]);
+
+    }
 }
